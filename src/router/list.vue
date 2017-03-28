c296cc91d8a5144a77237e9ee32f441f1e82fb1e
Lin-H <lh04667649@qq.com>
2017-03-28 23:40:43 +0800

    coding list style 50%

--- src/router/list.vue
+++ src/router/list.vue
@@ -1,9 +1,13 @@
 <template>
 <div class="v-lists">
-  <ul class="v-list" v-for="l in lists">
-    <h4>{{l.name}}</h4>
-    <li v-for="c in l.cards">{{c.name}}</li>
-  </ul>
+  <div class="wrap" :style="{width: lists.length * 280 + 'px'}">
+    <div class="v-list" v-for="l in lists">
+      <ul class="cards">
+        <h4>{{l.name}}</h4>
+        <li v-for="c in l.cards" class="card">{{c.name}}</li>
+      </ul>
+    </div>
+  </div>
 </div>
 </template>
 <script>
@@ -26,5 +30,29 @@ export default {
 </script>
 <style lang="sass">
 .v-lists
+  width: 100%;
+  overflow: auto;
+  .wrap
+    width: 600px
+    height: calc(100vh - 40px)
+    display: flex
+    padding: 10px
+  .v-list
+    flex: 1
+    .cards
+      background: #E2E4E6;
+      border-radius: 6px;
+      padding: 8px;
+      list-style: none;
+      margin-right: 8px;
+      .card:last-child
+        margin: 0
+    .card
+      background-color: white;
+      border-radius: 5px;
+      padding: 4px;
+      margin-bottom: 6px;
+    h4
+      margin: 0
 
 </style>
